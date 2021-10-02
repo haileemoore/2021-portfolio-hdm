5c4e9afd6fac8e76569efdb22e358a6ec0b8dcf1
Hailee Doyle <haileexdoyle@gmail.com>
2021-10-01 18:11:22 -0600

    cosmetics

--- hdm-portfolio/src/App.js
+++ hdm-portfolio/src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav className="App-nav">
+        <a href="#">home</a>
+        <a href="#">portfolio</a>
+        <a href="#">contact</a>
+      </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>WELCOME</h1>
-        <a className="App-link" href="#" target="_blank" rel="noopener noreferrer">learn more</a>
       </header>
     </div>
   );
