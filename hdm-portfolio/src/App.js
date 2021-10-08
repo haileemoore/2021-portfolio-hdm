d6781fac459486b9e997bdffac14aca5696127c5
Hailee Doyle <haileexdoyle@gmail.com>
2021-10-07 19:07:10 -0600

    cosmetic changes and animation tweaking

--- hdm-portfolio/src/App.js
+++ hdm-portfolio/src/App.js
@@ -1,20 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function PageLoad() {
   return (
-    <div className="App">
-      <nav className="App-nav">
-        <a href="#">home</a>
-        <a href="#">portfolio</a>
-        <a href="#">contact</a>
-      </nav>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>WELCOME</h1>
+    <div className="page-load">
+      <header className="page-header">
+        <h1>HELLO</h1>
+        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+          <animate attributeType="CSS" attributeName="visibility" from="visible" to="hidden" dur="1s" repeatCount="indefinite" />
+        </svg>
       </header>
     </div>
   );
 }
 
-export default App;
+export default PageLoad;
