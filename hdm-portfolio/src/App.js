5d27e651e8a80aa665f4fcf8b39160e4fcec8bd7
Hailee Doyle <haileexdoyle@gmail.com>
2021-11-18 18:28:18 -0700

    navbar construction

--- hdm-portfolio/src/App.js
+++ hdm-portfolio/src/App.js
@@ -2,13 +2,26 @@ import './App.css';
 
 function HomePage() {
   return (
-    <div className="home-page">
-      <nav className="nav">
-        <a href="/home">home</a>
-        <a href="/portfolio">portfolio</a>
-      </nav>
-      <header className="name">
-        <h1>HELLO</h1>
+    <div className="homepage">
+      <ul className="nav">
+        <li className="nav-title">
+          <a className="title nav-link" href="/#">HAILEE MOORE</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/home">HOME</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/portfolio">PORTFOLIO</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/about">ABOUT</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/contact">CONTACT</a>
+        </li>
+      </ul>
+      <header className="intro">
+        <h1>Denver-based UX Designer who's world revolves around user-centric design, and creative problem solving. </h1>
       </header>
     </div>
   );
